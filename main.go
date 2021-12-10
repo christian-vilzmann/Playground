8be651fb7ec885de0250c953b11bab3640cc9623
Christian Vilzmann <cvilzmann@googlemail.com>
2021-12-10 22:01:48 +0100

    update main -> we can calculate

--- main.go
+++ main.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+func addiere(x, y int) int {
+	return x + y
+}
+
 func main() {
 	fmt.Println("Hello World")
+	// let´s calculate
+	fmt.Println("das ist noch nicht das große 1x1.")
+	fmt.Println("2+3=", addiere(3, 2))
 }
